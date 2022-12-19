8734601857d95b14c4fa54f224903b2aae8afbe1
Akon2020 <akonkwaushindi@gmail.com>
2022-12-19 09:40:51 -0800

    puts commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -17,3 +17,4 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 #endif /* AKON2020 */
+
